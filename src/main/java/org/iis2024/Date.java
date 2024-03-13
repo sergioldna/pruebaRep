e1342afb0b964321f0c6599366ff506060e20647
sergioldna <sergioaldanagonzalez@gmail.com>
2024-03-13 13:49:53 +0100

    Owner changed Date.java

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -62,10 +62,10 @@ public class Date {
    */
 
   // Constructor modified by the owner
-  public Date(int dia, int mes, int año) {
-    this.day = dia;
-    this.month = mes;
-    this.year = año;
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
   }
 
   /**
